469ca115438b8e655140cab004444608f225099d
AmeliaTYR <53657436+AmeliaTYR@users.noreply.github.com>
2021-10-23 17:54:34 +0800

    Sign Alphabet Detection for MLDA DLW

---
+++ testPy.py
@@ -0,0 +1,12 @@
+WORKSPACE_PATH = 'Tensorflow/workspace'
+SCRIPTS_PATH = 'Tensorflow/scripts'
+APIMODEL_PATH = 'Tensorflow/models'
+ANNOTATION_PATH = WORKSPACE_PATH+'/annotations'
+IMAGE_PATH = WORKSPACE_PATH+'/images'
+MODEL_PATH = WORKSPACE_PATH+'/models'
+PRETRAINED_MODEL_PATH = WORKSPACE_PATH+'/pre-trained-models'
+CONFIG_PATH = MODEL_PATH+'/my_ssd_mobnet/pipeline.config'
+CHECKPOINT_PATH = MODEL_PATH+'/my_ssd_mobnet/'
+
+print({SCRIPTS_PATH + '/generate_tfrecord.py'} -x {IMAGE_PATH + '/train'} -l {ANNOTATION_PATH + '/label_map.pbtxt'} -o {ANNOTATION_PATH + '/train.record'})
+print(){SCRIPTS_PATH + '/generate_tfrecord.py'} -x{IMAGE_PATH + '/test'} -l {ANNOTATION_PATH + '/label_map.pbtxt'} -o {ANNOTATION_PATH + '/test.record'})
